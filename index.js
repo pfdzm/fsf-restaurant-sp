d7dbc4b49c7ed260d0013660c6b284549dbc67d2
SK26 <saul.lazzarini@gmail.com>
2019-11-26 20:58:21 +0100

    everything working

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
 const tables = [];
 const waitList = [];
 
@@ -16,24 +19,32 @@ app.get("/reserve", (req, res) =>
   res.sendFile(path.join(__dirname, "/reserve.html"))
 );
 
+app.get("/api/tables", (req, res) => res.json(tables));
+
+app.get("/api/waitlist", (req, res) => res.json(waitList));
+
 // Create New Reservation - takes in JSON input
 app.post("/api/tables", function(req, res) {
   try {
-    var newReservation = req.body;
+    console.log(req.body);
+    var newReservation = req.body || {};
 
     // Using a RegEx Pattern to remove spaces from newCharacter
     // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
-    newReservation.customerName = newReservation.name
+    newReservation.customerName = newReservation.customerName
       .replace(/\s+/g, "")
       .toLowerCase();
 
-    if (table.lenth() < 5) {
+    if (tables.length < 5) {
       tables.push(newReservation);
+      res.send("Added to tables!");
     } else {
       waitList.push(newReservation);
+      res.send("Added to waitlist!");
     }
   } catch (error) {
-    res.send(error);
+    console.log(error);
+    return res.json(error);
   }
 });
 
