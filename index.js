5f40752a4cf0fbf07d09a313dca3826fd867e7ac
SK26 <saul.lazzarini@gmail.com>
2019-11-26 19:47:24 +0100

    update

--- index.js
+++ index.js
@@ -6,10 +6,6 @@ const port = process.env.PORT || 3000;
 const tables = [];
 const waitList = [];
 
-// Sets up the Express app to handle data parsing
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-
 app.get("/", (req, res) => res.sendFile(path.join(__dirname, "/index.html")));
 
 app.get("/tables", (req, res) =>
@@ -22,19 +18,23 @@ app.get("/reserve", (req, res) =>
 
 // Create New Reservation - takes in JSON input
 app.post("/api/tables", function(req, res) {
-  // req.body hosts is equal to the JSON post sent from the user
-  // This works because of our body parsing middleware
-  var newReservation = req.body;
+  try {
+    var newReservation = req.body;
 
-  // Using a RegEx Pattern to remove spaces from newCharacter
-  // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
-  newReservation.customerName = newReservation.name
-    .replace(/\s+/g, "")
-    .toLowerCase();
+    // Using a RegEx Pattern to remove spaces from newCharacter
+    // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
+    newReservation.customerName = newReservation.name
+      .replace(/\s+/g, "")
+      .toLowerCase();
 
-  if (table.lenth() < 5) {
-    tables.push(newReservation);
-  } else waitList.push(newReservation);
+    if (table.lenth() < 5) {
+      tables.push(newReservation);
+    } else {
+      waitList.push(newReservation);
+    }
+  } catch (error) {
+    res.send(error);
+  }
 });
 
 app.listen(port, () => console.log(`App listening on port ${port}!`));
