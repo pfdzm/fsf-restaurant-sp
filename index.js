23852119e5db76fcd69654aa040b08e0b921c78f
Pablo Fernandez <pfdzm@users.noreply.github.com>
2019-11-26 19:12:05 +0100

    minor fixes

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const path = require("path");
 const express = require("express");
 const app = express();
-const port = 3000 || process.env.port;
+const port = process.env.PORT || 3000;
 
-app.get("/", (req, res) => res.sendFile(path.join(__dirname, "index.html")));
+app.get("/", (req, res) => res.sendFile(path.join(__dirname, "/index.html")));
 
 app.listen(port, () => console.log(`App listening on port ${port}!`));
